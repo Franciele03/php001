a3b5391cd79a4126fb3b330a89a2c88cd748b915
Fran <francielepa03@gmail.com>
2025-05-12 22:22:56 -0300

    novo

--- exercicios.php
+++ exercicios.php
@@ -14,4 +14,48 @@ $idade = 21; //int
 $sobrenome = "Araujo"; //string
 $animalEstimacao = false; //bool ou boolean
 
+echo "<br>";
+echo "Eu tenho $idade anos <br> ";
+echo 'Eu tenho $idade anos <br> ';
+echo 'Eu tenho ' . $idade . ' anos <br> ';
+echo "Eu tenho " . $idade . " anos <br> ";
+ 
+if($idade > 21){
+    echo "cuidado com o tigrinho";
+ 
+}else{
+    echo "você ainda tem chance";
+}
 ?>
+ 
+<?="ola";?>
+ 
+<?php
+ 
+$corFavorita = "Rosa";
+ 
+switch($corFavorita) {
+    case 'azul' :
+        echo "<br>Sua cor favorita é azul";
+        break;
+    case 'vermelho':
+        echo"<br>Sua cor favorita é vermelho";
+        break;
+    case 'amarelo':
+        echo"<br>Sua cor favorita é amarelo";
+    default:
+      echo"<br>Não sei sua cor favorita";
+      break;
+}
+ 
+$i = 0;
+while($i<10){
+    echo "<br>vambora";
+    $i = $i + 1;
+}
+ 
+for($i = 0; $i < 10; $i=$i=1){
+    echo "<br>vambora";
+}
+?>
+
